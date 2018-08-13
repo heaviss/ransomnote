d41cebc57c699f4c0734a706ded09d5cded84d64
Vladimir Seregin <31631@rambler.ru>
2018-08-13 19:56:30 +0700

    Remove HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RansomNote text="Hi!"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RansomNote from '@/components/RansomNote.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    RansomNote
   }
 }
 </script>
@@ -21,8 +20,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
